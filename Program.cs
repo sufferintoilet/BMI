3a05d660f1ccfa7a280e81dba7938d0ce35d8700
sufferintoilet <nguyenhuynhquocan1@gmail.com>
2023-12-11 00:07:24 +0900

    BMI

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+       double height;
+       double weight;
+
+       Console.WriteLine("Enter your height");
+       height = float.Parse(Console.ReadLine());
+       Console.WriteLine("Enter your weight");
+       weight = float.Parse(Console.ReadLine());
+       double bmi = weight / Math.Pow(height,2);
+       bmi = Math.Round(bmi, 1);
+
+       Console.Write("BMI: "+ bmi);
+
+        if (bmi<18)
+            Console.WriteLine("Underweight");
+        else if (bmi<25)
+            Console.WriteLine("Normal");
+        else if (bmi<30)
+            Console.WriteLine("Overweight");
+        else
+            Console.WriteLine("Obese");
+
+    }
+}
